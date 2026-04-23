c95a219f2b5fbb598ac996c92745d12d625136da
Al3xmm14 <molinaa757@gmail.com>
2026-04-23 01:38:20 -0600

    ADD(stylesTopBar/groupsConfig)

---
+++ styles/barStyle.py
@@ -0,0 +1,10 @@
+def get_bar_style():
+    bar_style = {
+    "background": "#00000000",
+    "margin": [10, 14, 10, 15],
+    "border_color": ["ffffff", "ffffff", "ffffff", "ffffff"],
+    "opacity": 1,
+    }    
+    return bar_style
+
+
---
+++ utils/groups.py
@@ -0,0 +1,12 @@
+def groupTemplate(Match):
+    totalGroups = 5
+    group_names = [
+        ("1", "⬤", []),
+        ("2", "⬤", []), 
+        ("3", "⬤", []), 
+        ("4", "⬤", []), 
+        # Multimedia
+        ("5", "⬤", [Match(wm_class="crx_pjibgclleladliembfgfagdaldikeohf"),
+                    Match(wm_class="crx_cinhimbnkkaeohfgghhklpknlkffjgod")]), 
+    ]
+    return group_names
